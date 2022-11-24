abf9ad0c04b9d31556a6b89cbd31b8482e3bc9a3
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-11-24 18:55:17 +0200

    mongodb primary setup done

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD
+
+const uri = `mongodb+srv://${db_user}:${db_password}@cluster0.uzyhqeg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri);
 app.get('/', (req, res) => {
     res.send('car-finder running on server')
 })
 app.listen(port, () => {
     console.log(`car-finder running properly on ${port}`)
 })
+
+
